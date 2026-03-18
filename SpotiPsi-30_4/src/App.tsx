687fba803da2fb64e17815ad04f0de8db9059181
unknown <30>
2026-03-18 09:52:39 +0200

    added favoritePage and AllSongsPage

--- SpotiPsi-30_4/src/App.tsx
+++ SpotiPsi-30_4/src/App.tsx
@@ -4,26 +4,36 @@ import Player from './components/Player/Player';
 import MainSection from './components/Main/MainSection';
 import { useEffect, useState } from 'react';
 import { fetchSongs } from './components/HelperFunctions/FetchSongs';
-
-const data = await fetchSongs()
+import { fetchFavoriteSongs } from './components/HelperFunctions/FetchFavorites';
+import AllSongsPage from './components/AllSongsPage/AllSongsPage';
+import FavoritePage from './components/FavoritePage/FavoritePage';
+const allSongs = await fetchSongs()
+const favoriteSongs = await fetchFavoriteSongs()
 function App() {
   const { classes } = useStyles();
   const [currentPage, setCurrentPage] = useState("songs");
-  const [songList,setSongList] = useState([])
-
+  const [songList, setSongList] = useState([])
+  const [favoriteSongsList, setFavoriteSongsList] = useState([])
   const changePage = (pageType: string) => {
     setCurrentPage(pageType);
   };
-  useEffect(() =>{
-  setSongList(data)
-  console.log(songList)
+  useEffect(() => {
+    setSongList(allSongs)
+    console.log(songList)
 
-  },[songList])
+  }, [songList])
+
+  useEffect(() => {
+    setFavoriteSongsList(favoriteSongs)
+    console.log(favoriteSongsList)
+  }, [favoriteSongsList])
   return (
     <div className={classes.body}>
       <Header />
       <MainSection changePage={changePage} />
       <Player />
+      <AllSongsPage songList={songList}/>
+      <FavoritePage songList={songList} favoriteSongs={favoriteSongsList}/>
     </div>
   );
 }

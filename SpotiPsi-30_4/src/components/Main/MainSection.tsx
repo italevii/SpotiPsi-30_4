be9caa233f60b81f9e87ba85569fa26168888f3e
unknown <30>
2026-03-18 10:26:05 +0200

    30/added moving between pages

--- SpotiPsi-30_4/src/components/Main/MainSection.tsx
+++ SpotiPsi-30_4/src/components/Main/MainSection.tsx
@@ -1,16 +1,21 @@
+import PageContent from "../PageContent/PageContent";
 import SideBar from "../Sidebar/Sidebar";
 import useStyles from "./MainSectionStyle";
 
 
 interface Props{
     changePage:(pageType:string) => MouseEventHandler<HTMLLIElement>
+    pageType:string;
+    songList:Array<Object>;
+    favoriteSongs:Array<string>
 }
-const MainSection = ({changePage}:Props) =>{
+const MainSection = ({changePage,pageType,songList,favoriteSongs}:Props) =>{
       const { classes } = useStyles();
 
     return(
         <div className={classes.MainSection}>
         <SideBar changePage = {changePage}/>
+        <PageContent pageType = {pageType} songList={songList} favoriteSongs = {favoriteSongs} />
         </div>
 
     )

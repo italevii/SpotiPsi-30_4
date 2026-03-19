eaa5c0f72fad97fec5e8ab91b60fe26edee14c21
4 <itamar.levi.1997@gmail.com>
2026-03-19 12:54:16 +0200

    player 1 2 and 3 are done

--- SpotiPsi-30_4/src/App.tsx
+++ SpotiPsi-30_4/src/App.tsx
@@ -7,6 +7,7 @@ import { fetchFavoriteSongs } from './assets/HelperFunctions/FetchFavorites';
 import { fetchPlaylists } from './assets/HelperFunctions/FetchPlaylists';
 import { BrowserRouter } from "react-router-dom";
 import MainSection from './components/Main/MainSection';
+import type { Song } from './assets/types';
 import type { Playlist } from './assets/types';
 interface FavoritesContextType {
   favoriteSongsList: string[];
@@ -20,12 +21,30 @@ interface PlayListsContextType {
   playLists: Array<Playlist>;
   setPlaylists: React.Dispatch<React.SetStateAction<Playlist[]>>;
 }
+interface PlayerContextType {
+  currentSong: Song | undefined;
+  setCurrentSong: React.Dispatch<React.SetStateAction<Song | undefined>>;
+
+  isPlaying: boolean;
+  setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>;
+
+  queue: Song[];
+  setQueue: React.Dispatch<React.SetStateAction<Song[]>>;
+
+  currentTime: number;
+  setCurrentTime: React.Dispatch<React.SetStateAction<number>>;
+
+  duration: number;
+  setDuration: React.Dispatch<React.SetStateAction<number>>;
+}
+
 const allSongs = await fetchSongs()
 const favoriteSongs = await fetchFavoriteSongs()
 const serverPlayLists = await fetchPlaylists()
 
 export const FavoritesContext = createContext<FavoritesContextType | null>(null);
 export const reRenderPlaylistsContext = createContext<reRenderPlaylistsContextType | null>(null);
+export const PlayerContext = createContext<PlayerContextType | null>(null);
 export const PlayListsContext = createContext<PlayListsContextType | null>(null);
 function App() {
   const { classes } = useStyles();
@@ -33,26 +52,34 @@ function App() {
   const [favoriteSongsList, setFavoriteSongsList] = useState<string[]>(favoriteSongs)
   const [playLists, setPlaylists] = useState(serverPlayLists)
   const [reRenderPlayList, setReRenderPlayList] = useState<boolean>(true)
+  const [currentSong, setCurrentSong] = useState<Song | undefined>(undefined);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [queue, setQueue] = useState<Song[]>([]);
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const [duration, setDuration] = useState<number>(0);
 
   useEffect(() => {
     setSongList(allSongs)
 
   }, [songList])
 
-  
+
   useEffect(() => {
     const fetchData = async () => {
     const serverPlayLists = await fetchPlaylists()
     setPlaylists(serverPlayLists)
   }
+
   fetchData()
   }, [reRenderPlayList])
 
 return (
   <BrowserRouter>
     <div className={classes.body}>
+    <PlayerContext.Provider
+    value={{currentSong, setCurrentSong, isPlaying, setIsPlaying, queue, setQueue, currentTime, setCurrentTime, duration, setDuration}}>
       <Header />
-      <PlayListsContext.Provider value = {{playLists,setPlaylists}}>
+    <PlayListsContext.Provider value = {{playLists,setPlaylists}}>
       <reRenderPlaylistsContext.Provider value={{reRenderPlayList, setReRenderPlayList}}>
         <FavoritesContext.Provider value={{ favoriteSongsList, setFavoriteSongsList }}>
           <MainSection songList={songList} playLists={playLists} />
@@ -60,9 +87,14 @@ return (
       </reRenderPlaylistsContext.Provider>
       </PlayListsContext.Provider>
       <Player/>
+        </PlayerContext.Provider>
     </div>
   </BrowserRouter>
 );
+
+
+
+
 
 }
 
--- SpotiPsi-30_4/src/components/Player/Player.tsx
+++ SpotiPsi-30_4/src/components/Player/Player.tsx
@@ -1,10 +1,45 @@
 import useStyles from "./PlayerStyle";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
+import Typography from '@mui/material/Typography';
+import { useContext } from "react";
+import { PlayerContext } from "../../App";
+
 const Footer = () => {
     const { classes } = useStyles();
+    const playerContext = useContext(PlayerContext);
+    if (!playerContext) return null;
+
+    const { currentSong, setCurrentSong, isPlaying, setIsPlaying, queue, setQueue, currentTime, setCurrentTime, duration, setDuration } = playerContext;
+
     return (
-        <>
-            <div className={classes.footer}>נגן שירים</div>
-        </>
+        <Box className={classes.footer} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 0.1 }}>
+            <Typography className={classes.song_name}>
+                {currentSong?.name} 
+            </Typography >
+            <Typography className={classes.artist_name}>
+                {currentSong?.artist} 
+            </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: "1vw" }}>
+                <SkipPreviousIcon className={classes.skip_icons} />
+                <PlayArrowIcon className={classes.player_icon} />
+                <SkipNextIcon className={classes.skip_icons} />
+            </Box>
+            <Slider
+                aria-label="time-indicator"
+                size="small"
+                value={currentTime}
+                min={0}
+                step={1}
+                max={duration}
+                onChange={(_, value) => setCurrentTime(value)}
+                className={classes.slider}
+            />
+        </Box>
     )
 }
 
